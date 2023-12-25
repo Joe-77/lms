f32fd3986cfdfeb81fbe786213e5d5934ff30ef0
Yousef <yousefabdallah55464@gmail.com>
2023-12-25 04:13:12 +0200

    update

--- src/firbase/firebaseConfig.ts
+++ src/firbase/firebaseConfig.ts
@@ -1,27 +1,27 @@
 import { initializeApp } from "firebase/app";
-import {  GoogleAuthProvider,  getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROGECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE,
-  messagingSenderId: import.meta.env.VITE_SENDER,
-  appId: import.meta.env.VITE_APP,
+  apiKey: "AIzaSyCuvgbSYOlcsoHj-Te3alyvXxIQBIGJfCg",
+  authDomain: "courses-88b02.firebaseapp.com",
+  projectId: "courses-88b02",
+  storageBucket: "courses-88b02.appspot.com",
+  messagingSenderId: "987148900122",
+  appId: "1:987148900122:web:b52c3753c2c3ca659bfea3",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 
-// storage 
+// storage
 
-export const storage = getStorage(app)
+export const storage = getStorage(app);
 
-// db 
+// db
 
-export const db = getFirestore(app)
+export const db = getFirestore(app);
